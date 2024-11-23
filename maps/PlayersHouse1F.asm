70cbaa07bb0be9c08e2b772b0bafb6314d6ee779
Jordan Moore <jordanmoore753@gmail.com>
2024-11-22 20:42:46 -0500

    set pokedex at start to speed up battling

--- maps/PlayersHouse1F.asm
+++ maps/PlayersHouse1F.asm
@@ -40,6 +40,7 @@ MeetMomScript:
 	scall PlayersHouse1FReceiveItemStd
 	setflag ENGINE_POKEGEAR
 	setflag ENGINE_PHONE_CARD
+	setflag ENGINE_POKEDEX
 	addcellnum PHONE_MOM
 	setscene SCENE_PLAYERSHOUSE1F_NOOP
 	setevent EVENT_PLAYERS_HOUSE_MOM_1
