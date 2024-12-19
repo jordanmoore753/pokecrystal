cdbd7b14236782293d4976c001610eadea8d2c0a
Jordan Moore <jordanmoore753@gmail.com>
2024-12-19 18:04:37 -0500

    remove unused routine

--- engine/pokemon/get_level_cap.asm
+++
@@ -1,7 +0,0 @@
-GetLevelCap:
-  call GetBadgeCount   ; Get the badge count (result in A)
-  ld hl, LevelCapsTable ; Load the start address of the level caps table
-  add a, l             ; Add badge count to the low byte of HL
-  ld l, a              ; Store the result back in L
-  ld a, [hl]           ; Load the level cap value from the table
-  ret                  ; Return with the level cap in A
