29c6140fc494f9808429c75dff944e25367b97a8
Jordan Moore <jordanmoore753@gmail.com>
2024-12-19 17:51:21 -0500

    include data in right spot

--- main.asm
+++ main.asm
@@ -40,7 +40,7 @@ INCLUDE "engine/events/bug_contest/caught_mon.asm"
 INCLUDE "engine/items/item_effects.asm"
 INCLUDE "engine/battle_anims/pokeball_wobble.asm"
 INCLUDE "engine/pokemon/knows_move.asm"
-
+INCLUDE "data/badge_level_caps.asm"
 
 SECTION "bank4", ROMX
 
