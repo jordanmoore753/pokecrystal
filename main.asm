739b40cd10695c25cc45e45ad162736d3397d25b
Jordan Moore <jordanmoore753@gmail.com>
2024-12-20 09:26:41 -0500

    move data to spot it works in

--- main.asm
+++ main.asm
@@ -13,7 +13,6 @@ INCLUDE "engine/overworld/npc_movement.asm"
 INCLUDE "engine/events/happiness_egg.asm"
 INCLUDE "engine/events/shuckle.asm"
 INCLUDE "engine/events/haircut.asm"
-INCLUDE "engine/pokemon/get_max_level.asm"
 
 
 SECTION "bank2", ROMX
@@ -683,6 +682,12 @@ SECTION "Crystal Events", ROMX
 
 INCLUDE "engine/events/battle_tower/load_trainer.asm"
 INCLUDE "engine/events/odd_egg.asm"
+
+
+SECTION "Level Caps", ROMX
+
+INCLUDE "engine/pokemon/get_max_level.asm"
+
 
 SECTION "Stadium 2 Checksums", ROMX[$7DE0], BANK[$7F]
 
